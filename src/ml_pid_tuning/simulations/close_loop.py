e71710653915784a60a2982eada90e22f3c81387
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2026-08-26 21:08:56 +0200

    chore: adjust open_loop to work with every given object

--- src/ml_pid_tuning/simulations/close_loop.py
+++ src/ml_pid_tuning/simulations/close_loop.py
@@ -4,7 +4,7 @@ from ml_pid_tuning.controllers.pid import PIDController
 from ml_pid_tuning.metrics.performance import calculate_iae, calculate_ise, calculate_itae, calculate_overshoot, calculate_settling_time
 import matplotlib.pyplot as plt
 
-def run_simulation(plant, pid, setpoint=1.0, simulation_time=20.0, dt=0.1):
+def run_simulation(plant, pid, setpoint=1.0, simulation_time=60.0, dt=0.1):
     current_time = 0.0
     step_number = 0
 
--- src/ml_pid_tuning/simulations/open_loop.py
+++ src/ml_pid_tuning/simulations/open_loop.py
@@ -1,20 +1,16 @@
 from ml_pid_tuning.plants.first_order import FirstOrderPlant
+from ml_pid_tuning.plants.second_order import SecondOrderPlant
+
 import matplotlib.pyplot as plt
 
-def run_simulation():
-    plant = FirstOrderPlant(gain=2.0, time_constant=5.0)
-
-    input_signal = 1.0
-    simulation_time = 60.0
-    dt = 0.1
-
+def run_simulation(plant, input_signal=1.0, simulation_time=60.0, dt=0.1):
     current_time = 0.0
     step_number = 0
 
     time_values = []
     output_values = []
 
-    print("Open-loop simulation of a first-order plant")
+    print("Open-loop simulation")
     print()
     print("Time [s]\tInput\tOutput")
 
@@ -34,11 +30,14 @@ def run_simulation():
 
     plt.xlabel("Time [s]")
     plt.ylabel("Output")
-    plt.title("First-order plant step response")
+    plt.title("Open-loop step response")
     plt.grid()
 
     plt.savefig("plots/step_response.png")
     plt.close()
 
 if __name__ == "__main__":
-    run_simulation()
+    plant1 = FirstOrderPlant(gain=2.0, time_constant=5.0)
+    plant2 = SecondOrderPlant(gain=2.0, time_constant_1=3.0, time_constant_2=5.0)
+
+    run_simulation(plant2)
